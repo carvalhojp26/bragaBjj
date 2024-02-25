95625fb9a916edbccdc7857562f84e21263cc149
Joao Paulo <acarvalho.jp@gmail.com>
2024-02-25 02:24:44 +0000

    Creating location page

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,54 @@ function rightClick() {
 document.addEventListener('DOMContentLoaded', leftClick);
 
 
+let map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'),{
+        center:{lat:41.53, lng:-8.41},
+        zoom:13
+    });
+    directionsRenderer.setMap(map); // Associa o DirectionsRenderer ao mapa
+    google.maps.event.addListener(map, "click", function(event){
+        this.setOptions({scrollWheel:true});
+    });
+}
+
+let directionsService = new google.maps.DirectionsService();
+let directionsRenderer = new google.maps.DirectionsRenderer();
+
+function getLocation() {
+    //inicializada quando aperta o botao
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
+
+const successCallback = (position) => {
+    console.log(position);
+    
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    
+    // Chama compareDistance 
+    compareDistance(latitude, longitude);
+};
+
+const errorCallback = (error) => {
+    console.error(error);
+};
+
+function compareDistance(latitude, longitude) {
+    let destinationLatLng = new google.maps.LatLng(41.53915966898499, -8.410085691669995); 
+    
+    let request = {
+        origin: new google.maps.LatLng(latitude, longitude),
+        destination: destinationLatLng, 
+        travelMode: "DRIVING",
+    };
+    
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+        } else {
+            console.error('Directions request failed due to ' + status);
+        }
+    });
+}
